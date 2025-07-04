88535f3cf8bb00a3a4646f099ba4879afee3e33d
Saikat <saikatchakraborty416@gmail.com>
2025-07-05 04:30:25 +0530

    .

--- components/Profile/ProfileSupportMessages.js
+++ components/Profile/ProfileSupportMessages.js
@@ -1,47 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const ProfileSupportMessages = ({topPayments}) => {
+const ProfileSupportMessages = ({ topPayments }) => {
   return (
-    <div className='h-auto md:h-[350px] bg-blue-500/25 p-3 rounded-md w-[95%] md:w-[45%]'>
-      <h3 className='text-2xl font-bold'>Top Supporters</h3>
-      {/* <div className='w-full h-full flex flex-col justify-center'> */}
-      {topPayments.map((payment)=>(
-        <div className='flex gap-1 items-center'>
-          <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-          <p className='truncate w-full '>{payment.senderName} donated &#8377;{payment.amount} with a message &quot;{payment.message}&quot;</p>
+    <div className="h-auto md:h-[350px] bg-blue-500/25 p-3 rounded-md w-[95%] md:w-[45%]">
+      <h3 className="text-2xl font-bold mb-2">Top Supporters</h3>
+      {topPayments.map((payment, index) => (
+        <div key={index} className="flex gap-2 items-center">
+          <img
+            className="w-[40px] h-[40px] rounded-full"
+            src="/avatar.gif"
+            alt={`${payment.senderName}'s avatar`}
+          />
+          <p className="truncate w-full text-sm">
+            <span className="font-semibold">{payment.senderName}</span> donated ₹{payment.amount} with a message &quot;{payment.message}&quot;
+          </p>
         </div>
       ))}
-      {/* <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>
-      <div className='flex gap-1 items-center'>
-        <img className='w-[40px] h-[40px] rounded-full' src={"/avatar.gif"}/>
-        <p>Shubham donated $30 with a message "Lots of love Saiiiii"</p>
-      </div>       */}
-      {/* </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSupportMessages
+export default ProfileSupportMessages;
