b404e4c54fc51dfbf3da7cb0dbed4372a342bf7a
Saikat <saikatchakraborty416@gmail.com>
2025-07-05 06:36:45 +0530

    .

--- app/layout.js
+++ app/layout.js
@@ -26,11 +26,13 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionWrapper>
-        <Navbar/>
-        <div className="relative min-h-[87vh] w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-            {children}        
-        </div>
-        <Footer/>
+          <Navbar />
+          <div className="relative min-h-[87vh] w-full bg-slate-950">
+            <div className="-z-0 absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+            <div className="-z-0 absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+            {children}
+          </div>
+          <Footer />
         </SessionWrapper>
       </body>
     </html>
